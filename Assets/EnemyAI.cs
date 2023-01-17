b12b55551d282ccb3738b9e97d09a69298d7b9c4
Sputnikboi <Guppymund@gmail.com>
2023-01-17 13:42:10 +0100

    changes

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float enemyMoveSpeed= 10;
+    public float enemyDamage=10;
+    public float enemyHp=10;
+    public Transform PlayerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public Transform[] spawnPositions;
-    public GameObject[] enemys;
+    public Transform Player;
+    public GameObject[] enemies;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,23 @@ public class EnemyScript : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0)){
             int randomSpawn= Random.Range(0,spawnPositions.Length);
-            Instantiate(enemys[0],spawnPositions[randomSpawn].position,transform.rotation);
+            Instantiate(enemies[0],spawnPositions[randomSpawn].position,transform.rotation);
         }
+
+
+        
+        spawnPositions[0].transform.position = new Vector3(Player.transform.position.x+15f,Player.transform.position.y+7f,8);
+        spawnPositions[1].transform.position = new Vector3(Player.transform.position.x-15f,Player.transform.position.y+7f,8);
+        spawnPositions[2].transform.position = new Vector3(Player.transform.position.x+15f,Player.transform.position.y-7f,8);
+        spawnPositions[3].transform.position = new Vector3(Player.transform.position.x-15f,Player.transform.position.y-7f,8);
+        spawnPositions[4].transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y+7f,8);
+        spawnPositions[5].transform.position = new Vector3(Player.transform.position.x+15f,Player.transform.position.y,8);
+        spawnPositions[6].transform.position = new Vector3(Player.transform.position.x-15f,Player.transform.position.y,8);
+        spawnPositions[7].transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y-7f,8);
+        spawnPositions[8].transform.position = new Vector3(Player.transform.position.x-7.5f,Player.transform.position.y-7f,8);
+        spawnPositions[9].transform.position = new Vector3(Player.transform.position.x+7.5f,Player.transform.position.y-7f,8);
+        spawnPositions[10].transform.position = new Vector3(Player.transform.position.x-7.5f,Player.transform.position.y+7f,8);
+        spawnPositions[11].transform.position = new Vector3(Player.transform.position.x+7.5f,Player.transform.position.y+7f,8);
+
     }
 }
